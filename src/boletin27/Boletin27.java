dfb12b1cebb0b420b5b7c9fddaf6b1d7cd865d44
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-26 14:36:14 +0200

    Boletin 27 de Programacion

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,11 @@
+package boletin27;
+
+import boletin27.aplicacion.Calculadora;
+
+public class Boletin27 {
+
+    public static void main(String[] args) {
+        Calculadora paginaPrincipal = new Calculadora();
+        paginaPrincipal.setVisible(true);
+    }  
+}
